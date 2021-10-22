56910bf666bd0d887998d9386f5259eb67b13a17
Joshua Lipps <joshsldevelopment@gmail.com>
2021-10-22 15:53:00 -0400

    app is now dockerized

---
+++ dockerfile
@@ -0,0 +1,13 @@
+FROM node:17
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package.json ./
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+EXPOSE 3000
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('../config.json');
 const bot = require('./bots/bot.js');
 
 // instantiate client and intents
